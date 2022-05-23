008bf1ca9edcb91e8b3ba3852b7c2f885a8ca6e5
Natanael Lima <taelima1997@gmail.com>
2022-05-22 22:20:58 -0300

    update scanner config android

--- android/app/src/main/java/com/camerascannerdocument/MainApplication.java
+++ android/app/src/main/java/com/camerascannerdocument/MainApplication.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import org.opencv.DocumentScannerPackage;
-import com.reactlibrary.RNPdfScannerPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
