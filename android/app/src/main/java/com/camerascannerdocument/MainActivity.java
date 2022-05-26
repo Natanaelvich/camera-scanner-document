dd4713cef98f233d0f5ec87c2076102393643724
Natanael Lima <taelima1997@gmail.com>
2022-05-25 23:05:21 -0300

    remove config android scanner

--- android/app/src/main/java/com/camerascannerdocument/MainActivity.java
+++ android/app/src/main/java/com/camerascannerdocument/MainActivity.java
@@ -3,7 +3,6 @@ package com.camerascannerdocument;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import com.documentscanner.DocumentScannerPackage;
 
 public class MainActivity extends ReactActivity {
 
--- android/app/src/main/java/com/camerascannerdocument/MainApplication.java
+++ android/app/src/main/java/com/camerascannerdocument/MainApplication.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
-import com.documentscanner.DocumentScannerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.config.ReactFeatureFlags;
