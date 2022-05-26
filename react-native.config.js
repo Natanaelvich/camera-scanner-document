9b45dfe87800b85b74d2cf5886b7e01cce0d6c22
Natanael Lima <taelima1997@gmail.com>
2022-05-25 23:05:12 -0300

    remove rn config file

--- react-native.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  dependencies: {
-    'react-native-document-scanner': {
-      platforms: {
-        android: null, // disable Android platform, other platforms will still autolink if provided
-      },
-    },
-  },
-};
